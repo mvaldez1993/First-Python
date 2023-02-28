172e7d8c9884ee2e22f54a3e223d99f3a6bd5c15
mariav1993 <>
2023-02-28 22:50:16 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print  ('Hello World')
+
+num1 = 10
+num2 = 20
+
+num3 = num1 + num2
+print (num3)
+
+message = 'Computer Literacy 101'
+message = 'Computer Literacy 101'
+message = 'Computer Literacy 101'
+message = 'Computer Literacy 101'
+print(message)
